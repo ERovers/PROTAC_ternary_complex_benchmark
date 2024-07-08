b415260f1df83f92583b0a2a0570e3937663a82f
ERovers <110251556+ERovers@users.noreply.github.com>
2024-07-08 14:17:05 -0400

    upload files

---
+++ PROTAC_screening/7JTO_ALF/alphafold_job.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --job-name=7JTO_AF
+#SBATCH --account=def-schapira    
+#SBATCH --time=08:00:00           
+#SBATCH --cpus-per-task=8         
+#SBATCH --gres=gpu:1              
+#SBATCH --mem=20G                 
+
+# Load modules dependencies.
+module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 cuda/11.4 cudnn/8.2.0 kalign/2.03 hmmer/3.2.1 openmm-alphafold/7.5.1 hh-suite/3.3.0 python/3.8.10
+
+DOWNLOAD_DIR=/datashare/alphafold   
+INPUT_DIR=$SCRATCH/AF_PROTAC_screening/7JTO_ALF     
+OUTPUT_DIR=${SCRATCH}/AF_PROTAC_screening/7JTO_ALF 
+
+# Generate your virtual environment in $SLURM_TMPDIR.
+virtualenv --no-download ${SLURM_TMPDIR}/env
+source ${SLURM_TMPDIR}/env/bin/activate
+
+# Install AlphaFold and its dependencies.
+pip install --no-index --upgrade pip
+pip install --no-index --requirement ~/alphafold-requirements.txt
+
+# Edit with the proper arguments and run your commands.
+# run_alphafold.py --help
+run_alphafold.py \
+   --fasta_paths=${INPUT_DIR}/7JTO.fasta \
+   --output_dir=${OUTPUT_DIR} \
+   --data_dir=${DOWNLOAD_DIR} \
+   --db_preset=full_dbs \
+   --model_preset=multimer \
+   --bfd_database_path=${DOWNLOAD_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+   --mgnify_database_path=${DOWNLOAD_DIR}/mgnify/mgy_clusters_2022_05.fa \
+   --template_mmcif_dir=${DOWNLOAD_DIR}/pdb_mmcif/mmcif_files \
+   --obsolete_pdbs_path=${DOWNLOAD_DIR}/pdb_mmcif/obsolete.dat \
+   --pdb_seqres_database_path=${DOWNLOAD_DIR}/pdb_seqres/pdb_seqres.txt \
+   --uniprot_database_path=${DOWNLOAD_DIR}/uniprot/uniprot.fasta \
+   --uniref30_database_path=${DOWNLOAD_DIR}/uniref30/UniRef30_2021_03 \
+   --uniref90_database_path=${DOWNLOAD_DIR}/uniref90/uniref90.fasta \
+   --hhblits_binary_path=${EBROOTHHMINSUITE}/bin/hhblits \
+   --hhsearch_binary_path=${EBROOTHHMINSUITE}/bin/hhsearch \
+   --jackhmmer_binary_path=${EBROOTHMMER}/bin/jackhmmer \
+   --kalign_binary_path=${EBROOTKALIGN}/bin/kalign \
+   --max_template_date=2020-08-17 \
+   --use_gpu_relax=True
